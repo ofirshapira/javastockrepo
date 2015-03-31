a2c56a86267d4fc88a09a8aa49cabbc7c37af70b
ofirshapira <ofir.shapira1@gmail.com>
2015-03-31 23:18:21 +0300

    EX02 SimpleMath

--- src/com/shapira/JavastockrepoServlet.java
+++ src/com/shapira/JavastockrepoServlet.java
@@ -7,7 +7,13 @@ import javax.servlet.http.*;
 public class JavastockrepoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1 = 4;
+		int num2 = 3;
+		int num3 = 7;
+		int result = num3 * (num1 + num2);
+		String resultStr = new String( "<h1>Result of "+num3+ " * ("+num1+" + "+num2+") ="+result+"</h1>");
+				
+resp.getWriter().println(resultStr);
 	}
 }
